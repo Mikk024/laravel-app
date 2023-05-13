51d9188220b130d99b0ea8d73f1eb796f753448b
Mikolaj Godek <mikolajgodek024@gmail.com>
2023-05-13 20:49:58 +0200

    create relationship Make with CarModel

--- app/Models/CarModel.php
+++ app/Models/CarModel.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class CarModel extends Model
 {
     use HasFactory;
+
+
+    public function make()
+    {
+        return $this->belongsTo(Make::class);
+    }
 }
--- app/Models/Make.php
+++ app/Models/Make.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Make extends Model
 {
     use HasFactory;
+
+    public function model()
+    {
+        return $this->hasMany(CarModel::class);
+    }
 }
