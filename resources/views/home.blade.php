b1ae32812665c5eb1c98147c14d1d115feee76c2
Mikolaj Godek <mikolajgodek024@gmail.com>
2023-05-14 23:32:12 +0200

    edit home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,93 +4,35 @@
 <div class="px-10 mt-10">
     <div class="card px-6 shadow-lg">
         <p class="text-center text-lg capitalize py-4">what are you looking for?</p>
-        <form action="" class="mb-10">
-            <div class="px-10 flex flex-row justify-around pb-4">
-                <label for="body" class="capitalize">
-                    body style
-                    <select name="body" class="block px-6 py-2 bg-gray-200 rounded-md">
-                        <option selected>Choose body style</option>
-                        @foreach ($body as $item)
-                            <option value="{{ $item }}">{{ $item }}</option>
-                        @endforeach
-                    </select>
-                </label>
-                <label for="make" class="capitalize">
-                    make
-                    <select name="make" class="block px-6 py-2 bg-gray-200 rounded-md">
-                        <option selected>Choose make</option>
-                        <option>Mercedes</option>
-                        <option>BMW</option>
-                        <option>Volkswagen</option>
-                    </select>
-                </label>
-            </div>
-            <div class="px-10 flex flex-row justify-around pb-4">
-                <label for="model" class="capitalize">
-                    model
-                    <select name="model" class="block px-6 py-2 bg-gray-200 rounded-md">
-                        <option selected>Choose model</option>
-                        <option>E class</option>
-                        <option>S class</option>
-                        <option>C class</option>
-                        <option>5 Series</option>
-                        <option>3 Series</option>
-                    </select>
-                </label>
-                <label for="fuel" class="capitalize">
-                    fuel
-                    <select name="fuel" class="block px-6 py-2 bg-gray-200 rounded-md">
-                        <option selected>Choose fuel</option>
-                        @foreach ($fuel as $item)
-                            <option value="{{ $item }}">{{ $item }}</option>                            
-                        @endforeach
-                    </select>
-                </label>
-            </div>
-            <div class="px-10 flex flex-row justify-around pb-4">
-                <label for="price" class="capitalize">
-                    price
+        <form action="">
+            <div class="flex justify-around mb-10">
+                <div>
+                    <label for="body" class="text-lg capitalize">body style</label>
                     <div>
-                        <select name="model" class="inline-block px-6 py-2 bg-gray-200 rounded-md">
-                        <option selected>From</option>
-                        @for ($i = 0; $i < 10001; $i+= 500)
-                            <option value="{{ $i }}">{{ $i }}</option>
-                        @endfor
-                        @for ($i = 10000; $i < 50001; $i+= 5000)
-                            <option value="{{ $i }}">{{ $i }}</option>
-                        @endfor
-                        </select>
-                        <select name="model" class="inline-block px-6 py-2 bg-gray-200 rounded-md">
-                            <option selected>From</option>
-                            @for ($i = 0; $i < 10001; $i+= 500)
-                                <option value="{{ $i }}">{{ $i }}</option>
-                            @endfor
-                            @for ($i = 10000; $i < 50001; $i+= 5000)
-                                <option value="{{ $i }}">{{ $i }}</option>
-                            @endfor
+                        <select class="bg-gray-200 px-4 py-1 rounded-md">
+                                <option value="" selected>Choose body style</option>
+                               @foreach ($body as $item)
+                                   <option value="{{ $item }}">{{ $item }}</option>
+                               @endforeach
                         </select>
                     </div>
-                </label>
-                <label for="price" class="capitalize">
-                    year of production
+                </div> 
+                <div>
+                    <label for="fuel" class="text-lg capitalize">fuel</label>
                     <div>
-                        <select name="model" class="inline-block px-6 py-2 bg-gray-200 rounded-md">
-                            <option selected>From</option>
-                            @for ($i = 1980; $i < 2024; $i++)
-                            <option value="{{ $i }}">{{ $i }}</option>    
-                            @endfor
-                        </select>
-                        <select name="model" class="inline-block px-6 py-2 bg-gray-200 rounded-md">
-                            <option selected>From</option>
-                            @for ($i = 1980; $i < 2024; $i++)
-                            <option value="{{ $i }}">{{ $i }}</option>    
-                            @endfor
+                        <select class="bg-gray-200 px-4 py-1 rounded-md">
+                                <option value="" selected>Choose fuel</option>
+                                @foreach ($fuel as $item)
+                                    <option value="{{ $item }}">{{ $item}}</option>
+                                @endforeach
                         </select>
                     </div>
-                </label>
+                </div>
             </div>
-            <button class="bg-red-300 px-6 py-2 rounded-md hover:bg-red-500 capitalize mx-auto justify-center flex">search</button>
-        </form> 
+            <livewire:cardropdown />
+            <button class="bg-red-300 px-6 py-2 rounded-md hover:bg-red-500 capitalize mx-auto justify-center flex mb-10">search</button>
+        </form>
+        
     </div>
 </div>
 @endsection
