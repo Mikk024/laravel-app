701811b846f7eeb59cdd6f8525d37b3f28824217
Mikolaj Godek <mikolajgodek024@gmail.com>
2023-05-16 22:07:33 +0200

    create StoreListing request wtih validation

---
+++ app/Http/Requests/StoreListingRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\CarBody;
+use App\Enums\CarFuel;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreListingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'make' => ['required', 'string'],
+            'model' => ['required', 'string'],
+            'fuel' => ['required', 'string', Rule::in(CarFuel::getValues())],
+            'year' => ['required', 'integer'],
+            'body' => ['required', 'string', Rule::in(CarBody::getValues())],
+            'horsepower' => ['required', 'integer'],
+            'capacity' => ['required', 'integer'],
+            'doors' => ['required', 'integer','between:1,7'],
+            'color' => ['required', 'string'],
+            'transmission' => ['required', 'string', Rule::in(['automatic', 'manual'])],
+        ];
+    }
+}
