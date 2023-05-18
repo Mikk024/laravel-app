f42e8d57685744755efecbbcf05ecbc50ecfb1af
Mikolaj Godek <mikolajgodek024@gmail.com>
2023-05-18 17:41:54 +0200

    edit controller add enum values to create function and make store function

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -2,12 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\CarBody;
+use App\Enums\CarColor;
+use App\Enums\CarFuel;
+use App\Http\Requests\StoreListingRequest;
+use App\Models\Listing;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Str;
+use Carbon\Carbon;
 class ListingController extends Controller
 {
     public function create()
     {
-        return view('listings.create');
+        $fuel = CarFuel::getValues();
+
+        $body = CarBody::getValues();
+
+        $currentYear = Carbon::now()->format('Y');
+
+        $color = CarColor::getValues();
+
+        return view('listings.create', [
+            'fuel' => $fuel,
+            'body' => $body,
+            'currentYear' => $currentYear,
+            'color' => $color
+        ]);
+    }
+
+    public function store(StoreListingRequest $request)
+    {        
+       $validated = $request->validated();
+
+       $validated['user_id'] = 1;
+
+       if ($request->has('images')) {
+        $randomString = Str::random();
+        foreach ($request->file('images') as $image) {
+            $path[] = $image->store('listings/' . $randomString);
+        }
+        $validated['images'] = $path;
+       }
+
+       Listing::create($validated);
+
+       return redirect('/');
     }
 }
