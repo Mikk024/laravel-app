77f45f5309e37d624b885fe34681715321043a47
Mikolaj Godek <mikolajgodek024@gmail.com>
2023-05-19 21:07:52 +0200

    add route names to navbar and set route name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,12 +11,12 @@
 <body>
     <nav class="shadow-md sticky top-0 z-50 capitalize bg-gray-300">
         <div class="container px-10 py-4 flex justify-between">
-            <a href="#">home</a>
+            <a href="{{ route('home')}}">home</a>
             <div class="space-x-2">
                 <a href="#" class="hover:underline">login</a>
                 <span>/</span>
                 <a href="#" class="hover:underline">register</a>
-                <a href="#" class="px-10 py-2 rounded-md  bg-blue-300 hover:bg-blue-500 hover:text-white">add listing</a>
+                <a href="{{ route('listing.create')}}" class="px-10 py-2 rounded-md  bg-blue-300 hover:bg-blue-500 hover:text-white">add listing</a>
             </div>
             
         </div>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::prefix('listing')->group(function () {
     Route::get('/create', [ListingController::class, 'create'])->name('listing.create');
