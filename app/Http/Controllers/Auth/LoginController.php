02e1525107a488bcbf1d70e37eb07196c9cada67
Mikolaj Godek <mikolajgodek024@gmail.com>
2023-05-20 22:09:28 +0200

    add create and store function to Login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginUserRequest;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    public function store(LoginUserRequest $request)
+    {
+        $credentials = ($request->validated());
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+ 
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
 }
