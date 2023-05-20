5818d726a2529a8d918f08a6bd1843961291940e
Mikolaj Godek <mikolajgodek024@gmail.com>
2023-05-20 18:50:18 +0200

    add create and store function to RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterUserRequest;
 
 class RegisterController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    public function store(RegisterUserRequest $request)
+    {
+        $validated = $request->validated();
+
+        User::create($validated);
+
+        return redirect('/');
+    }
 }
