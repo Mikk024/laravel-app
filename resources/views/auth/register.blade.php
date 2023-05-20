b306361aad25f2f530e527290be55cee4d3d44d4
Mikolaj Godek <mikolajgodek024@gmail.com>
2023-05-20 18:08:10 +0200

    create register view

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="mt-32 px-14 mb-20">
+        <div>
+            <p class="text-4xl capitalize text-center">register</p>
+            <div class="bg-gray-100 rounded-md px-14 py-6 mt-6 w-1/2 mx-auto text-xl text-center flex-row justify-center">
+                <form action="{{ route('register.store')}}" method="POST">
+                    @csrf
+                    <label for="email" class="capitalize">email</label>
+                    <input type="email" name="email" class="block mx-auto w-1/2 mb-6 mt-2 rounded-md bg-gray-200">
+                    <label for="name" class="capitalize">username</label>
+                    <input type="text" name="name" class="block mx-auto w-1/2 mb-6 mt-2 rounded-md bg-gray-200">
+                    <label for="password" class="capitalize">password</label>
+                    <input type="password" name="password" class="block mx-auto w-1/2 mb-6 mt-2 rounded-md bg-gray-200">
+                    <label for="password_confirmation" class="capitalize">confirm password</label>
+                    <input type="password" name="password_confirmation" class="block mx-auto w-1/2 mb-6 mt-2 rounded-md bg-gray-200">
+                    <button type="submit" class="px-6 py-2 capitalize bg-red-500 text-white rounded-lg mt-10 hover:bg-red-700 hover:text-black">register</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
