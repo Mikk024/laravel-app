2052c1ef2dd015c08be27be52a8e6ae5043208ca
Mikolaj Godek <mikolajgodek024@gmail.com>
2023-05-20 18:48:40 +0200

    add setPasswordAttribute to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Listing::class);
     }
+
+    public function setPasswordAttribute($value)
+    {
+    $this->attributes['password'] = bcrypt($value);
+    }
 }
