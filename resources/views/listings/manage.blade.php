8d25f81900b6f08e840674c4256f4336acfb896a
Mikolaj Godek <mikolajgodek024@gmail.com>
2023-05-21 22:46:57 +0200

    create manage view

---
+++ resources/views/listings/manage.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="px-32 mt-10">
+        <p class="text-3xl capitalize text-center">manage your listings</p>
+        <div class="flex justify-center">
+            <table class="table-auto mt-10 text-xl capitalize w-full text-center px-32">
+                <thead>
+                <tr>
+                    <th>id</th>
+                    <th>title</th>
+                    <th>actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($listings as $listing)
+                <tr>
+                    <td>{{ $listing->id }}</td>
+                    <td><a href="{{ route('listings.show', ['id' => $listing->id])}}" class="hover:underline">{{ $listing->year . ' ' . $listing->make->name . ' ' . $listing->model->name }}</a></td>
+                    <td class="space-x-2">
+                        <form action="" class="inline-block">
+                            <button class="capitalize px-4 py-2 bg-red-500 rounded-md text-white hover:bg-red-700">delete</button>
+                        </form>
+                        <a href="#" class="capitalize inline-block px-4 py-2 bg-green-500 rounded-md text-white hover:bg-green-700">edit</a>
+                    </td>
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
