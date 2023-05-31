b874f3ee54b2cc861442e1846aaa46b0040f02b8
Mikolaj Godek <mikolajgodek024@gmail.com>
2023-05-31 22:26:23 +0200

    add names to livewire dropdown component

--- resources/views/livewire/cardropdown.blade.php
+++ resources/views/livewire/cardropdown.blade.php
@@ -2,7 +2,7 @@
     <div>
         <label for="make" class="text-lg capitalize">make</label>
         <div>
-            <select wire:model="selectedMake" class="bg-gray-200 px-4 py-1 rounded-md">
+            <select wire:model="selectedMake" class="bg-gray-200 px-4 py-1 rounded-md" name="make">
                     <option value="" selected>Choose make</option>
                     @foreach ($makes as $make)
                         <option value="{{ $make->id }}">{{ $make->name }}</option>
@@ -13,10 +13,10 @@
     <div>
         <label for="model" class="text-lg capitalize">model</label>
         <div>
-            <select class="bg-gray-200 px-4 py-1 rounded-md">
+            <select class="bg-gray-200 px-4 py-1 rounded-md" name="model">
                     <option value="" selected>Choose model</option>
                    @foreach ($models as $model)
-                        <option>{{ $model->name }}</option>
+                        <option value="{{ $model->id }}">{{ $model->name }}</option>
                    @endforeach
             </select>
         </div>
