7a27b3f176a25126539cad7174dc931cd1cdece7
Mikolaj Godek <mikolajgodek024@gmail.com>
2023-06-26 21:21:18 +0200

    set up tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,14 +4,22 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Database\Seeders\MakeSeeder;
+use Database\Seeders\ModelSeeder;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
 
 class ExampleTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+        $this->seed([MakeSeeder::class, ModelSeeder::class]);
+
         $response = $this->get('/');
 
         $response->assertStatus(200);
